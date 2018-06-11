79671def25fdecfe1a9f284eb08cee080f8a95c4
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2018-06-11 16:29:26 +0200

    added monkeys

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
+import monkey
 if __name__ == "__main__":
     print("Hello world")
     print("Starting to understand some git, this is already my second commit!")
     print("Hellow world again")
     print("i think i already udnerstood the basic concept of add and commit")
+    monkey.print_monkeys([0,1,2,3,4,5])
