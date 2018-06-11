9f95b163dcd46164bb6e076ea72b8b64c0a924e2
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2018-06-11 15:41:21 +0200

    renamed back

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+if __name__ == "__main__":
+    print("Hello world")
+    print("Starting to understand some git, this is already my second commit!")
+    print("Hellow world again")
+    print("i think i already udnerstood the basic concept of add and commit")
--- hello_world2.py
+++
@@ -1,4 +0,0 @@
-# -*- coding:utf-8 -*-
-if __name__ == "__main__":
-    print("Hello world")
-    print("Starting to understand some git, this is already my second commit!")
