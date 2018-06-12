f8f4d30079708ff004e6b3af97257c65ab9ef816
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2018-06-12 11:26:18 +0200

    added rabbits

--- hello_world.py
+++ hello_world.py
@@ -10,10 +10,22 @@ def print_palmtrees(num):
         sys.stdout.write('  | ')
         sys.stdout.write('\n\n')
 
+def print_rabbit_range(num):
+    for i in range(num):
+        sys.stdout.write(" \\/")
+    sys.stdout.write("\n")
+    for i in range(num):
+        sys.stdout.write(" 00 ")
+    sys.stdout.write("\n")
+    for i in range(num):
+        sys.stdout.write(" ** ")
+    sys.stdout.write("\n")
+
 if __name__ == "__main__":
-    print("Hello world")
+    print("Bye monkeys")
     print("Starting to understand some git, this is already my second commit!")
     print("Hellow world again")
     print("i think i already udnerstood the basic concept of add and commit")
     monkey.print_monkeys([0,1,2,3,4,5])
     print_palmtrees(5)
+    print_rabbit_range(5)
