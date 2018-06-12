d5f6c3c62d4add77a5208ce863cfc8359874d772
Thomas Dencker <thomas.dencker@stud.uni-goettingen.de>
2018-06-12 10:40:37 +0200

    palm trees

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,19 @@
 # -*- coding:utf-8 -*-
 import monkey
+import sys
+
+def print_palmtrees(num):
+    for i in range(num):
+        sys.stdout.write(' /|\\')
+        sys.stdout.write('\n')
+    for i in range(num):
+        sys.stdout.write('  | ')
+        sys.stdout.write('\n\n')
+
 if __name__ == "__main__":
     print("Hello world")
     print("Starting to understand some git, this is already my second commit!")
     print("Hellow world again")
     print("i think i already udnerstood the basic concept of add and commit")
     monkey.print_monkeys([0,1,2,3,4,5])
+    print_palmtrees(5)
